5f46ab8119df22d09ae6cdb7f1892b887de992ad
Adam Zlatohlavek <zlatohlavecek@gmail.com>
2023-05-21 19:15:52 +0200

    cors for server api

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const ListRouter = require('./routes/lists/ListsRouter');
 
 const app = new Koa();
 
 app.use(bodyParser());
+app.use(cors());
 
 app.use(ListRouter.routes())
     .use(ListRouter.allowedMethods());

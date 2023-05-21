1b31214e01771e7a973d8d104880969a2839b26c
Adam Zlatohlavek <zlatohlavecek@gmail.com>
2023-05-21 18:01:22 +0200

    change to monorepo and eslint config

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest', sourceType: 'module', project: ['./tsconfig.json'], tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/prop-types': [0],
+    'react/require-default-props': [0],
+    'max-len': ['error', 120],
+    'no-param-reassign': ['error', { props: false }],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
